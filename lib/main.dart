f2bc8d1fab9c492f26beb9e7ac072ce809571399
eder6 <eder@b2mlportal.com.br>
2021-08-13 13:35:50 -0300

    Atualização

--- lib/main.dart
+++ lib/main.dart
@@ -12,15 +12,16 @@ Future<void> main() async {
   runApp(MaterialApp(
     home: Home(),
     theme: ThemeData(
-        hintColor: Colors.amber,
-        primaryColor: Colors.white,
-        inputDecorationTheme: InputDecorationTheme(
-          enabledBorder:
-              OutlineInputBorder(borderSide: BorderSide(color: Colors.white)),
-          focusedBorder:
-              OutlineInputBorder(borderSide: BorderSide(color: Colors.amber)),
-          hintStyle: TextStyle(color: Colors.amber),
-        )),
+      hintColor: Colors.white,
+      primaryColor: Colors.white,
+      // inputDecorationTheme: InputDecorationTheme(
+      //   enabledBorder:
+      //       OutlineInputBorder(borderSide: BorderSide(color: Colors.white)),
+      //   focusedBorder:
+      //       OutlineInputBorder(borderSide: BorderSide(color: Colors.amber)),
+      //   hintStyle: TextStyle(color: Colors.amber),
+      // )
+    ),
   ));
 }
 
@@ -30,6 +31,48 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  double dolar;
+  double euro;
+  final realController = TextEditingController();
+  final dolarController = TextEditingController();
+  final euroController = TextEditingController();
+
+  void realChange(String text) {
+      if(text.isEmpty) {
+      _clearAll();
+      return;
+    }
+    double real = double.parse(text);
+    dolarController.text = (real / dolar).toStringAsPrecision(2);
+    euroController.text = (real / euro).toStringAsPrecision(2);
+  }
+
+  void dolarChange(String text) {
+      if(text.isEmpty) {
+      _clearAll();
+      return;
+    }
+    double dolar = double.parse(text);
+    realController.text = (dolar * this.dolar).toStringAsPrecision(2);
+    euroController.text = (dolar * this.dolar / euro).toStringAsPrecision(2);
+  }
+
+  void euroChange(String text) {
+      if(text.isEmpty) {
+      _clearAll();
+      return;
+    }
+   double euro = double.parse(text);
+    realController.text = (euro * this.euro).toStringAsPrecision(2);
+    dolarController.text = (euro * this.euro / dolar).toStringAsPrecision(2);
+  }
+
+  void _clearAll(){
+    realController.text = "";
+    dolarController.text = "";
+    euroController.text = "";
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -62,13 +105,55 @@ class _HomeState extends State<Home> {
                   ),
                 );
               } else {
-                return Container(color: Colors.green);
+                dolar = snapshot.data["results"]["currencies"]["USD"]["buy"];
+                euro = snapshot.data["results"]["currencies"]["EUR"]["buy"];
+                return SingleChildScrollView(
+                  padding: EdgeInsets.all(10.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      Icon(
+                        Icons.monetization_on,
+                        size: 125.0,
+                        color: Colors.amber[900],
+                      ),
+                      buildTexField("Reais", "R\$", realController, realChange),
+                      Divider(),
+                      buildTexField(
+                          "Dolar", "US\$", dolarController, dolarChange),
+                      Divider(),
+                      buildTexField("euro", "€", euroController, euroChange),
+                    ],
+                  ),
+                );
               }
           }
         },
       ),
     );
   }
+}
+
+buildTexField(
+    String label, String prefix, TextEditingController c, Function change) {
+// "€"
+  return TextField(
+    controller: c,
+    decoration: InputDecoration(
+      labelText: label,
+      labelStyle: TextStyle(
+        color: Colors.white,
+      ),
+      border: OutlineInputBorder(),
+      prefixText: prefix,
+    ),
+    style: TextStyle(
+      color: Colors.white,
+      fontSize: 25.0,
+    ),
+    onChanged: change,
+   keyboardType: TextInputType.numberWithOptions(decimal: true),
+  );
 }
 
 Future<Map> getData() async {
